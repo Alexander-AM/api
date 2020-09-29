52c3a49b746c2ac5d886b9fd9fca7985903cf8dc
4480135 <4480135@RTS-365.dk>
2020-09-29 12:09:21 +0200

    Fix file-bucket paths.

--- controllers/asset.controller.js
+++ controllers/asset.controller.js
@@ -1,11 +1,12 @@
 var { Asset } = require("../models/models");
 var saveFile = require("../services/asset");
+const aboutController = require("./about.controller");
 
 async function createSingleAsset(req, res, next) {
 	try {
 		let file = saveFile(req.files.file);
 		let asset = await Asset.create({
-			url: "http://localhost:4000/file-bucket/" + file
+			url: "https://dyrevelfaerd-alexander.herokuapp.com/file-bucket/" + file
 		});
 		res.json(asset);
 	} catch (error) {
@@ -33,6 +34,17 @@ async function getSingleAsset(req, res, next) {
 		res.status(500).end();
 	}
 }
+
+/*async function patchAllAssets() {
+    let assets = await Asset.findAll();
+
+    for(let i = 0; i < assets.length; i++) {
+        assets[i].url = assets[i].url.replace("http://localhost:4000/", "https://dyrevelfaerd-alexander.herokuapp.com/");
+        await assets[i].save();
+    }
+}
+
+patchAllAssets();*/
 
 module.exports = {
 	createSingleAsset,
